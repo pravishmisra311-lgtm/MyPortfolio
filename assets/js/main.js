f11b62bd4814a498b74748c160e67297e6ba5375
pravishmisra311-lgtm <pravishmisra311@gmail.com>
2026-07-06 23:54:11 +0530

    scroll bar,scroll up and active link maintained

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,34 @@ const sendEmail = async (e) => {
     }
 }
 contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+ : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+// Link the ID of each section (section id="home") to each link (a href="#home") 
+// and activate the link with the class .active-link
+const scrollActive = () => {
+   // We get the position by scrolling down
+   const scrollY = window.scrollY
+
+   sections.forEach(section => {
+      const id = section.id, // id of each section
+            top = section.offsetTop - 50, // Distance from the top edge
+            height = section.offsetHeight, // Element height
+            link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+      if(!link) return
+
+      link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+   })
+}
+window.addEventListener('scroll', scrollActive)
